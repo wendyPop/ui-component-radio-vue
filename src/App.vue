788560de3164fed27ffad920bd9a375b50d7cba5
wendyPop <wishbong@gmail.com>
2022-06-26 16:01:43 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <section>
+      <radio-button
+          :items="radioData"
+          :name="'가격필터'"
+          :selected="selected"
+          @check="clickPriceInfo"
+      />
+      <pre>
+        <code>
+          {{ priceSelected }}
+        </code>
+      </pre>
+    </section>
+  </div>
+</template>
+
+<script>
+import RadioButton from './components/RadioButton.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      radioData: [
+        {
+          "nm": "전체",
+          "min_dis_price": null,
+          "max_dis_price": null,
+          "is_editable": false
+        },
+        {
+          "nm": "5만원 이하",
+          "min_dis_price": 0,
+          "max_dis_price": 50000,
+          "is_editable": false
+        },
+        {
+          "nm": "5만원 ~ 10만원 이하",
+          "min_dis_price": 50000,
+          "max_dis_price": 100000,
+          "is_editable": false
+        },
+        {
+          "nm": "10만원 ~ 50만원 이하",
+          "min_dis_price": 100000,
+          "max_dis_price": 500000,
+          "is_editable": false
+        },
+        {
+          "nm": "50만원 ~ 100만원 이하",
+          "min_dis_price": 500000,
+          "max_dis_price": 1000000,
+          "is_editable": false
+        }
+      ],
+      selected: 0,
+      priceSelected: {
+        name: '',
+        minPrice: null,
+        maxPrice: null,
+      }
+    }
+  },
+  components: {
+    RadioButton
+  },
+  methods: {
+    clickPriceInfo(value) {
+      // 초기화
+      this.priceSelected.name = ''
+      this.priceSelected.minPrice = null
+      this.priceSelected.maxPrice = null
+
+      // 데이터 세팅
+      const selected = this.radioData.find(v => v.nm === value)
+      this.priceSelected.name = selected.nm
+      this.priceSelected.minPrice = selected.min_dis_price
+      this.priceSelected.maxPrice = selected.max_dis_price
+    }
+  }
+}
+</script>
---
+++ src/components/RadioButton.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<div class="radio_box" v-for="(item, idx) in items" :key="idx">
+			<input
+				type="radio"
+				:name="name"
+				:id="item.nm"
+				class="radio_check"
+				:checked="idx === selected"
+				:value="item.nm"
+				@change="radioCheck"
+			/>
+			<label :for="item.nm" class="radio_type"> {{ item.nm }} </label>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RadioButton',
+	props: {
+		name: {
+			type: String,
+			required: true,
+		},
+		items: {
+			type: Array,
+			default: () => {},
+		},
+		selected: {
+			type: Number,
+			default: 0,
+		},
+	},
+	methods: {
+		radioCheck(e) {
+			this.$emit('check', e.target.value)
+		},
+	},
+}
+</script>
+
+<style lang="scss">
+@import './src/assets/styles/Form.scss';
+</style>
